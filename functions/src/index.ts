a0848947f68ee8e54294e02bac8bc125454fd45a
cloudliner <cloudliner.jp@gmail.com>
2018-03-21 21:23:31 +0900

    addMessage()

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +8,10 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const addMessage = functions.https.onRequest((req, res) => {
+  const original = req.query.text;
+  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+    res.redirect(303, snapshot.ref);
+  });
+});
